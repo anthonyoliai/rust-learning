c6d3ac9ae8f2a1a0eb8893a584d403d4f10b6618
anthonyoliai <anthonyoliai@gmail.com>
2023-07-30 01:03:23 -0700

    app: hello_cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-97c804fb0741aa2c.d
@@ -0,0 +1,5 @@
+/home/temp/rust-learning/hello_cargo/target/debug/deps/hello_cargo-97c804fb0741aa2c: src/main.rs
+
+/home/temp/rust-learning/hello_cargo/target/debug/deps/hello_cargo-97c804fb0741aa2c.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/temp/rust-learning/hello_cargo/target/debug/hello_cargo: /home/temp/rust-learning/hello_cargo/src/main.rs
