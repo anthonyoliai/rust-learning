0d3fc6089e451f78494906262b5501f12af0eb57
Anthony Oliai <61785803+anthonyoliai@users.noreply.github.com>
2023-07-30 10:25:44 +0200

    Delete hello_cargo/target directory

--- hello_cargo/target/debug/deps/hello_cargo-97c804fb0741aa2c.d
+++
@@ -1,5 +0,0 @@
-/home/temp/rust-learning/hello_cargo/target/debug/deps/hello_cargo-97c804fb0741aa2c: src/main.rs
-
-/home/temp/rust-learning/hello_cargo/target/debug/deps/hello_cargo-97c804fb0741aa2c.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/temp/rust-learning/hello_cargo/target/debug/hello_cargo: /home/temp/rust-learning/hello_cargo/src/main.rs
