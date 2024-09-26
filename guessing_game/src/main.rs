0c287d450d984db4e0724bc2d1170739289cfb12
anthonyoliai <anthonyoliai@gmail.com>
2024-09-26 18:33:08 +0200

    gg:cleanup

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,33 +3,29 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+    println!("Welcome, to the guessing game!");
+    println!("Please choose a number between 1 and 100.");
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("Guess the number");
 
     loop {
-        println!("Please input your guess.");
         let mut guess = String::new();
+        println!("Please input your guess.");
+
         io::stdin()
             .read_line(&mut guess)
-            .expect("Failed to read input");
+            .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(a) => a,
-            Err(_) => {
-                println!("Failed to read input");
-                continue;
-            }
-        };
-
-        println!("You guessed: {guess}");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too small"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You got it!");
                 break;
             }
+            Ordering::Greater => println!("Too large"),
         }
     }
 }
